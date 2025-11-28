a2a23da1414a5d5d3be7afdc6cf09849ca5a39d6
algoga <algoga@alumnatflorida.es>
2025-11-28 09:53:55 +0100

    Añadiendo ejercicio 2

---
+++ 2.php
@@ -0,0 +1,23 @@
+<?php
+class Average {
+    private $num1;
+    private $num2;
+    private $num3;
+
+    function __construct($n1, $n2, $n3){
+        $this->num1=$n1;
+        $this->num2=$n2;
+        $this->num3=$n3;
+    }
+
+    public function media(){
+        $primero=$this->num1;
+        $segundo=$this->num2;
+        $tercero=$this->num3;
+        $media = ($primero + $segundo + $tercero) / 3;
+        return $media;
+    }
+
+}
+$primeraMedia = new Average(80, 20, 10);
+echo "La media de estos tres números es: " . $primeraMedia->media();
